66c2f13fde351902d03f4c268cf2c7ee86ca7550
alejojad <alejandrojad2003@gmail.com>
2025-09-12 14:10:00 -0400

    First Commit

---
+++ InflationRate3.cpp
@@ -0,0 +1,173 @@
+//Alejandro Jad
+//September 16, 2022
+//Inflation rate Lab 1 part 2
+
+#include <iostream>
+using namespace std;
+
+void getCPIValues(float&, float&);
+float getInflationRate(float, float);
+void sort_array(float[], int);
+void swap_values(float&, float&);
+float findMedianRate(float[], int);
+//void print_array(float[], int);
+const int max_size = 20;
+
+
+int main()
+{
+	//variables
+	float ratesArray[max_size], oldCpi, newCpi, inflationRate, numberOfRepeats, averageRate, median;
+	char repeat;
+	int i, arraySize;
+	
+	
+	//initialization
+	numberOfRepeats = 0;
+	averageRate = 0;
+	i = 0;
+	arraySize = 0;
+
+	//input
+	do
+	{
+		
+		getCPIValues(oldCpi, newCpi);
+		inflationRate = getInflationRate(oldCpi, newCpi);
+	
+		if(i < max_size)
+		{
+			ratesArray[i] = inflationRate;
+			i++;
+			arraySize++;
+		}		
+		//output
+		cout << "Inflation rate is " << inflationRate << endl;
+		cout << "Try again? (y or n): ";
+		cin >>	repeat;
+	
+		//calculation to find total number of repeats, and total rate
+		numberOfRepeats = numberOfRepeats + 1;
+		averageRate = averageRate + inflationRate;
+	}
+	while(repeat == 'y');
+	
+	//formula to find the average rate:
+	if(numberOfRepeats > 0)
+	{
+		averageRate = averageRate / numberOfRepeats;
+		cout << "Average rate is " << averageRate << endl;
+	}	
+	
+	//function to find median 
+	median = findMedianRate(ratesArray, arraySize);
+	cout << "Median rate is " << median << endl;
+	
+	//function to print array
+	//print_array(ratesArray, arraySize);
+				
+	return 0;
+}
+
+void getCPIValues(float &old, float &newCpi)
+{		
+	do
+	{
+		cout << "Enter the old and new consumer price indices: ";
+		cin >> old >> newCpi;
+
+		if(old <=  0)
+		{
+			cout << "Error: CPI Values Must be greater than 0" << endl;
+			cout << "Enter the old and new consumer price indices: ";
+			cin >> old >> newCpi;	
+		}
+		if (newCpi <= 0)
+		{		
+			cout << "Error: CPI Values Must be greater than 0" << endl;
+			cout << "Enter the old and new consumer price indices: ";
+			cin >> old >> newCpi;
+		}
+	}
+	while(old <=0 && newCpi <= 0);
+
+}
+float getInflationRate(float old, float newCpi)
+{	
+	if (old <=  0)
+	{
+		cout << "Error: CPI Values Must be greater than 0" << endl;
+		cout << "Enter the old and new consumer price indices: ";
+		cin >> old >> newCpi;
+	}	
+	if (newCpi <= 0)
+	{
+		cout << "Error: CPI Values Must be greater than 0" << endl;
+		cout << "Enter the old and new consumer price indices: ";
+		cin >> old >> newCpi;
+	}
+	
+
+	//calculating inflation rate
+	return  ( ( newCpi - old ) / old ) * 100;
+}
+
+void swap_values(float &a, float &b)
+{
+
+	float temp;
+	
+	temp = a; 
+	a = b;
+	b = temp;
+
+}
+
+void sort_array(float r[], int size)
+{
+	int maxElement, index;
+	for(maxElement = size - 1; maxElement > 0; maxElement--)
+	{
+		for(index = 0; index < maxElement; index++)
+		{
+			if(r[index] > r[index + 1])
+			{
+				swap_values(r[index], r[index + 1]);
+			}
+		}
+	}
+}
+
+//void print_array(float r[], int size)
+//{
+//	int i;
+	
+//	cout << "Sorted inflation rates" << endl;
+//	for(i = 0; i <  size; i++)
+//	{
+//		cout << r[i] << endl;
+//	}
+
+	
+//}
+float findMedianRate(float r[], int size) 
+{
+	int index;
+	float median;
+	
+	//function to sort the array
+	sort_array(r, size);
+
+	if(size % 2 == 0)
+	{
+		index = size / 2;
+		median = (r[index - 1] + r[index])/2;
+	}
+	else
+	{
+		index = size / 2;
+		median = r[index];
+	}
+
+	return median;
+}
---
+++ Marathon.cpp
@@ -0,0 +1,132 @@
+//Alejandro jad
+//November 2, 2022
+//Lab 7 Marathon times
+#include <iostream>
+#include <limits>
+using namespace std;
+
+//Declaring and defining struct timer
+struct Timer
+{
+	
+	int hours;
+	int minutes;
+	int seconds;
+};
+
+//Two parameters that are timers, both type timer
+//Compares the hours, then minutes, then seconds
+//Returns the difference between the two timers
+int compareTimes(Timer timer1, Timer timer2)
+{
+	int diff = timer1.hours - timer2.hours;
+    if (diff != 0) return diff;
+
+    diff = timer1.minutes - timer2.minutes;
+    if (diff != 0) return diff;
+
+    diff = timer1.seconds - timer2.seconds; 
+    return diff;
+}
+
+//parameter: Pointer to array of timers
+//parameter: size of array
+//Parameter: lowest timer= call by reference
+//parameter highest timer= call by reference
+//Find the minimum and maximum Timer in array of Timers
+void findMinMaxTimer(Timer* array, int size, Timer& lowest, Timer& highest)
+{   
+    lowest = array[0];
+    highest = array[0];
+
+    for (int i = 1; i < size; i++)
+    {
+        int dLow = compareTimes(lowest, array[i]);
+        if (dLow > 0)
+        {
+            lowest = array[i]; 
+        }
+
+        int dHigh = compareTimes(highest, array[i]);
+        if (dHigh < 0)
+        {
+            highest = array[i]; 
+        }
+    }
+
+}
+//Paramater: array of Timers, type Timer
+//Parameter: size, type int
+//checks to see if inputs are valid
+//returns Timer inputs to the array
+void readTimer(Timer* array, int size)
+{
+	char aChar;
+	for(int i = 0; i < size; i++)
+	{
+		cout << "Enter a Timer as hours:minutes:seconds(h:m:s)? ";
+		cin >> array[i].hours >> aChar >> array[i].minutes >> aChar >> array[i].seconds;
+		
+		while(true)
+		{
+			cout << "Invalid Timer!" << endl;
+			cout << "Enter a Timer as hours:minutes:seconds(h:m:s)? ";
+			cin >> array[i].hours >> aChar >> array[i].minutes >> aChar >> array[i].seconds;
+			
+			while(array[i].seconds > 59)
+			{
+				cout << "Invalid Timer!" << endl;
+				cout << "Enter a Timer as hours:minutes:seconds(h:m:s)? ";
+				cin >> array[i].hours >> aChar >> array[i].minutes >> aChar >> array[i].seconds;
+			}
+		}
+	}
+}
+//Parameter: size pf the array
+//returns pointer to array
+Timer* createTimerArray(int& size)
+{
+	Timer *arrayPtr= NULL;
+	
+	do
+	{
+		cout << "How many Timers do you need? ";//asking for size of the array
+		cin >> size;
+		if(size < 0)//check to see if value is valid
+		{
+			cout << "Enter a positive value!" << endl;
+		}
+	}
+	while(size < 0);
+	arrayPtr = new Timer [size];
+	
+	readTimer(arrayPtr, size);
+		
+	return arrayPtr;
+}
+
+
+
+int main()
+{
+	int sizeOfArray = 0;
+	Timer timer;
+	Timer *ptr;
+	Timer lowestTimer;
+	Timer highestTimer;
+    
+    
+	ptr = createTimerArray(sizeOfArray);
+	
+	lowestTimer = ptr[0];
+	highestTimer = ptr[0];	
+	findMinMaxTimer(ptr, sizeOfArray, lowestTimer, highestTimer);
+	
+	cout << "The minimum Timer is: " << lowestTimer.hours << ":" << lowestTimer.minutes << ":" << lowestTimer.seconds << endl;
+	cout << "The maximum Timer is: " << highestTimer.hours << ":" << highestTimer.minutes << ":" << highestTimer.seconds << endl;
+	
+
+	return 0;
+}	
+
+
